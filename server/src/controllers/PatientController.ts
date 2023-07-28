4685a8f272f6a2fd3ef029a1121279ff194c8e25
Rafael Strack <strackinado@gmail.com>
2023-07-28 13:48:17 -0300

    Controllers: list patients and get patient by id

--- server/src/controllers/PatientController.ts
+++ server/src/controllers/PatientController.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express'
 import { container } from '../containers/container'
 import { CreatePatientService } from '../services/CreatePatientService'
+import { ListPatientsService } from '../services/ListPatientsService'
+import { ListPatientsRequest } from '../dtos/listPatientsDTO'
+import { GetPatientByIdService } from '../services/GetPatientByIdService'
 
 export class PatientController {
   async create(req: Request, res: Response): Promise<Response> {
@@ -15,6 +18,42 @@ export class PatientController {
         address,
       })
       return res.status(201).json(result)
+    } catch (e) {
+      console.log(e)
+      return res.status(400).json({
+        error: true,
+        message: String(e),
+      })
+    }
+  }
+
+  async list(req: Request, res: Response): Promise<Response> {
+    try {
+      const { page, length, input } = req.query
+      const service = container.resolve(ListPatientsService)
+      const result = await service.execute({
+        page,
+        length,
+        input,
+      } as ListPatientsRequest)
+      return res.status(200).json(result)
+    } catch (e) {
+      console.log(e)
+      return res.status(400).json({
+        error: true,
+        message: String(e),
+      })
+    }
+  }
+
+  async getByPatientId(req: Request, res: Response): Promise<Response> {
+    try {
+      const { patientId } = req.params
+      const service = container.resolve(GetPatientByIdService)
+      const result = await service.execute({
+        patientId,
+      })
+      return res.status(200).json(result)
     } catch (e) {
       console.log(e)
       return res.status(400).json({

a61e919560704ea06d60b1c69ac580c705483c00
Rafael Strack <strackinado@gmail.com>
2023-07-30 18:38:06 -0300

    React hook form controlled date picker component

---
+++ client/src/components/ControlledDatePicker/ControlledDatePicker.tsx
@@ -0,0 +1,75 @@
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
+import {
+  Controller,
+  FieldPath,
+  FieldValues,
+  RegisterOptions,
+  useFormContext,
+} from 'react-hook-form'
+
+type ControlledDatePickerProps = {
+  name: string
+  label: string
+  defaultValue?: number | string
+  rules?: Omit<
+    RegisterOptions<FieldValues, FieldPath<FieldValues>>,
+    'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'
+  >
+  disablePast?: boolean
+  disableFuture?: boolean
+  minDate?: any
+  maxDate?: any
+}
+
+const ControlledDatePicker = ({
+  name,
+  label,
+  defaultValue,
+  rules,
+  disablePast,
+  disableFuture,
+  minDate,
+  maxDate,
+  ...rest
+}: ControlledDatePickerProps) => {
+  const { control, formState, getFieldState } = useFormContext()
+  const getErrorMsg = () => {
+    const { error } = getFieldState(name, formState)
+    return {
+      hasError: !!error,
+      message: error?.message || '',
+    }
+  }
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={rules}
+      defaultValue={null}
+      render={({ field: { value, onChange } }) => (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <DatePicker
+            {...rest}
+            format="dd/MM/yyyy"
+            label={label}
+            value={value || null}
+            onChange={onChange}
+            slotProps={{
+              textField: {
+                error: getErrorMsg().hasError,
+                helperText: getErrorMsg().message,
+              },
+            }}
+            disablePast={disablePast}
+            disableFuture={disableFuture}
+            minDate={minDate}
+            maxDate={maxDate}
+          />
+        </LocalizationProvider>
+      )}
+    />
+  )
+}
+
+export default ControlledDatePicker

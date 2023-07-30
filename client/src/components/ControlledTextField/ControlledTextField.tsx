5124ebd36b16066e72924c100cd4e2a68f3e28e6
Rafael Strack <strackinado@gmail.com>
2023-07-30 16:30:22 -0300

    React hook form controlled text field component

---
+++ client/src/components/ControlledTextField/ControlledTextField.tsx
@@ -0,0 +1,54 @@
+import { TextField } from '@mui/material'
+import {
+  Controller,
+  FieldPath,
+  FieldValues,
+  RegisterOptions,
+  useFormContext,
+} from 'react-hook-form'
+
+type ControlledTextFieldProps = {
+  name: string
+  label: string
+  defaultValue?: number | string
+  rules?: Omit<
+    RegisterOptions<FieldValues, FieldPath<FieldValues>>,
+    'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'
+  >
+}
+
+const ControlledTextField = ({
+  name,
+  label,
+  defaultValue,
+  rules,
+  ...rest
+}: ControlledTextFieldProps) => {
+  const { control, formState, getFieldState } = useFormContext()
+  const getErrorMsg = () => {
+    const { error } = getFieldState(name, formState)
+    return {
+      hasError: !!error,
+      message: error?.message || '',
+    }
+  }
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={rules}
+      render={({ field: { value, onChange } }) => (
+        <TextField
+          {...rest}
+          label={label}
+          value={value || ''}
+          onChange={(event) => onChange(event.target.value)}
+          error={getErrorMsg().hasError}
+          helperText={getErrorMsg().message}
+        />
+      )}
+    />
+  )
+}
+
+export default ControlledTextField

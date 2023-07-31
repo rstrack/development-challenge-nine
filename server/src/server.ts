5f4ad94a912c3cdf2ca2d7121736a9f8c8a7b855
Rafael Strack <strackinado@gmail.com>
2023-07-31 19:14:29 -0300

    Helmet and CORS config

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,22 @@
 import 'dotenv/config'
 import cors from 'cors'
 import express, { json } from 'express'
+import helmet from 'helmet'
 import { routes } from './routes/routes'
+
+const allowedOrigins = process.env.CORS_ALLOWED_ORIGINS || ''
+const allowedOriginsArray = allowedOrigins.split(',')
 
 const app = express()
 
 app.use(json())
-app.use(cors())
+app.use(helmet())
+app.use(
+  cors({
+    origin: allowedOriginsArray,
+  })
+)
+
 app.use(routes)
 
 app.listen(process.env.PORT, () => {
